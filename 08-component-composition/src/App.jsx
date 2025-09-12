eafc383d8ab00dba48857fc0c795bf5c2a3ff72c
ephraim <ephraims.818@gmail.com>
2025-09-13 04:38:39 +0600

    REACT--COMPLETE-REACT---COMPONENTS-JSX-PROPS-STATE - 08. Component Composition

---
+++ 08-component-composition/src/App.jsx
@@ -0,0 +1,38 @@
+import { CORE_CONCEPTS } from "./data";
+import { CoreConcepts } from "./components/CoreConcepts";
+import { Header } from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx.jsx";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcepts {...CORE_CONCEPTS[1]} />
+                        <CoreConcepts {...CORE_CONCEPTS[2]} />
+                        <CoreConcepts {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton>Components</TabButton>
+                        <TabButton>JSX</TabButton>
+                        <TabButton>Props</TabButton>
+                        <TabButton>State</TabButton>
+                    </menu>
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default App;
