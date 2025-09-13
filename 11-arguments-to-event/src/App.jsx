24279a66f24b7a030e3f476a43b9375987e91699
ephraim <ephraims.818@gmail.com>
2025-09-14 02:07:38 +0600

    REACT--COMPLETE-REACT---COMPONENTS-JSX-PROPS-STATE - 11. Passing Custom Arguments to Event Functions

---
+++ 11-arguments-to-event/src/App.jsx
@@ -0,0 +1,51 @@
+import { CORE_CONCEPTS } from "./data";
+import { CoreConcepts } from "./components/CoreConcepts";
+import { Header } from "./components/Header/Header.jsx";
+import TabButton from "./components/TabButton.jsx.jsx";
+
+function App() {
+    function handleSelect(selectedButton) {
+        console.log(selectedButton);
+    }
+
+    return (
+        <div>
+            <Header />
+            <main>
+                <section id="core-concepts">
+                    <h2>Core Concepts</h2>
+                    <ul>
+                        <CoreConcepts
+                            title={CORE_CONCEPTS[0].title}
+                            description={CORE_CONCEPTS[0].description}
+                            image={CORE_CONCEPTS[0].image}
+                        />
+                        <CoreConcepts {...CORE_CONCEPTS[1]} />
+                        <CoreConcepts {...CORE_CONCEPTS[2]} />
+                        <CoreConcepts {...CORE_CONCEPTS[3]} />
+                    </ul>
+                </section>
+                <section id="examples">
+                    <h2>Examples</h2>
+                    <menu>
+                        <TabButton onSelect={() => handleSelect("components")}>
+                            Components
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("jsx")}>
+                            JSX
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("props")}>
+                            Props
+                        </TabButton>
+                        <TabButton onSelect={() => handleSelect("state")}>
+                            State
+                        </TabButton>
+                    </menu>
+                    Dynamic Content
+                </section>
+            </main>
+        </div>
+    );
+}
+
+export default App;
